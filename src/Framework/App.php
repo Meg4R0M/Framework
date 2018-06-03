a06729cba715172b17be7c3b880e8364b19996db
Meg4R0M <psykoterro@gmail.com>
2018-06-03 22:17:32 +0200

    add base files

---
+++ src/Framework/App.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * @author : meg4r0m
+ * Date: 03/06/18
+ * Time: 21:11
+ */
+
+namespace Framework;
+
+use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Class App
+ * @package Framework
+ */
+class App
+{
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function run(ServerRequestInterface $request): ResponseInterface
+    {
+        $uri = $request->getUri()->getPath();
+        if (!empty($uri) && $uri[-1] === "/") {
+            return (new Response())
+                ->withStatus(301)
+                ->withHeader('Location', substr($uri, 0, -1));
+        }
+        if ($uri === '/blog') {
+            return new Response(200, [], '<h1>Bienvenue sur le blog</h1>');
+        }
+        return new Response(404, [], '<h1>Erreur 404</h1>');
+    }
+}
